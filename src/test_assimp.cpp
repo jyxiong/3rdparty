8ca0323305cc171d0faf976ffe14553f678ad11c
jyxiong <jingyu.xiong@foxmail.com>
2022-06-19 22:14:22 +0800

    添加 assimp

---
+++ src/test_assimp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "assimp/Importer.hpp"
+#include "assimp/Exporter.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+
+int main() {
+
+#ifndef TEST_PATH
+    std::cout << "Failed to add definitions of test path!" << std::endl;
+    return -1;
+#endif
+    std::string testPath = TEST_PATH;
+
+    // Create an instance of the Importer class
+    Assimp::Importer importer;
+
+    // And have it read the given file with some example postprocessing
+    // Usually - if speed is not the most important aspect for you - you'll
+    // probably to request more postprocessing than we do in this example.
+    const aiScene *scene = importer.ReadFile(testPath + "/assets/spider.obj",
+                                             aiProcess_CalcTangentSpace |
+                                             aiProcess_Triangulate |
+                                             aiProcess_JoinIdenticalVertices |
+                                             aiProcess_SortByPType);
+
+    // check for errors
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
+    {
+        std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+        return -1;
+    }
+
+    Assimp::Exporter exporter;
+    exporter.Export(scene, "obj", testPath + "/assets/spider_out.obj");
+
+    return 0;
+}
